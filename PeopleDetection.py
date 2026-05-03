68032a3edd9b76ff9809b79eddaf2c976fdee3c4
kapooraditi79 <thinktank7997@gmail.com>
2026-05-03 18:32:52 +0530

    video person okay

--- PeopleDetection.py
+++ PeopleDetection.py
@@ -12,6 +12,22 @@ cv2.imshow('People Detection', annotated_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+# # for the video 
+# cap= cv2.VideoCapture('testVideo/test2.mp4')
+
+# while True:
+#     ret, frame= cap.read()
+#     result= model(frame, classes=0)
+#     annotated_frame= result[0].plot()
+
+#     cv2.imshow('People Detection', annotated_frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# cap.release()
+# cv2.destroyAllWindows()
+
 # people detected successfully!!
 
 # now we will use the depth pro model
