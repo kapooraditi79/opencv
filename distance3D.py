57ddc6f24fb2fa45ebe777b3ed538a138679e551
kapooraditi79 <thinktank7997@gmail.com>
2026-05-04 17:13:07 +0530

    updating results

--- distance3D.py
+++ distance3D.py
@@ -1,3 +1,4 @@
+from sklearn.metrics._scorer import metric
 import sys
 sys.path.append(r'D:\Antares')  
 from pinhole import get_3d_coordinates
@@ -5,6 +6,7 @@ from PeopleDetection import detect_people
 from depthpro import get_depth_image, get_feet_depth
 import numpy as np
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.cluster import DBSCAN
 
 
 def get_distance(position_3d):
@@ -15,12 +17,18 @@ def get_distance(position_3d):
     # finding pairwise euclidean distance
     dist_matrix= euclidean_distances(coords, coords)
 
+    # cluster
+    clusters= DBSCAN(eps=0.8, min_samples=3,metric= 'precomputed')
+    labels= clusters.fit_predict(dist_matrix)    
+
     for i in range(len(ids)):
         for j in range(i+1, len(ids)):
             dist= dist_matrix[i][j]
             print(f"Person {ids[i]} <-> Person {ids[j]}: {dist:.2f}m")
     
-    return dist_matrix, ids
+    print('Labels are: ', labels)
+    
+    return dist_matrix, ids, labels
 
 
 if __name__=='__main__':
@@ -28,6 +36,6 @@ if __name__=='__main__':
     person_boxes= detect_people('testImage/test6.png')
     feet_depths= get_feet_depth(depth, person_boxes)
     position_3d= get_3d_coordinates(person_boxes, feet_depths, focallength, depth)
-    dist_matrix, ids= get_distance(position_3d)
+    dist_matrix, ids, labels= get_distance(position_3d)
     
     
