d234abd3163f3b7c5179a29b743dc4df5e66a87b
kapooraditi79 <thinktank7997@gmail.com>
2026-05-04 15:58:43 +0530

    convert 2d to 3d pixels- pinhole camera

--- depthpro.py
+++ depthpro.py
@@ -1,9 +1,8 @@
 import sys
 sys.path.append(r'D:\Antares')  
-
 import depth_pro
 from PIL import Image       # for image handling
-import torch
+import torch 
 import matplotlib.pyplot as plt
 from depth_pro import create_model_and_transforms
 import numpy as np
@@ -12,57 +11,29 @@ from PeopleDetection import detect_people
 # we will use gpu
 device= torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# load model and preprocessing transform
-model, transform= depth_pro.create_model_and_transforms()
-# move model to gpu
+# load once at module level
+model, transform = depth_pro.create_model_and_transforms()
 model.to(device)
-
-# turn on eval
-# to turn off dropout, training randomness
 model.eval()
 
-# load the image
-image, _, f_px = depth_pro.load_rgb('testImage/test6.png')
-# f_px: focal length of camera in pixel units
-# for converting depth → real-world scale
 
-# now transform
-image = transform(image).to(device)
+def get_depth_image(image_path):
+    image, _, f_px = depth_pro.load_rgb(image_path)
+    # f_px: focal length of camera in pixel units
+    # for converting depth → real-world scale``
+    # now transform
+    image = transform(image).to(device)
+    # Run inference.
+    # feeds image into model
+    # uses f_px for scale-aware depth
+    with torch.no_grad():
+        prediction = model.infer(image, f_px=f_px)
+    # extract depth
+    depth = prediction["depth"].cpu().numpy()  # Depth in [m].   # move the depth to cpu
+    # each pixel ->  depth[y][x] = distance from camera (in meters)
+    focallength_px = prediction["focallength_px"].cpu().numpy()  # Focal length in pixels.
+    return depth, focallength_px
 
-# Run inference.
-
-# feeds image into model
-# uses f_px for scale-aware depth
-with torch.no_grad():
-    prediction = model.infer(image, f_px=f_px)
-
-# extract depth
-depth = prediction["depth"].cpu().numpy()  # Depth in [m].   # move the depth to cpu
-# each pixel ->  depth[y][x] = distance from camera (in meters)
- 
-focallength_px = prediction["focallength_px"].cpu().numpy()  # Focal length in pixels.
-
-# showing depth map
-plt.figure(figsize=(10,5))
-plt.title("Depth Map (meters)")
-plt.imshow(depth, cmap='inferno')   # red = close, blue = far
-plt.colorbar(label="Depth (m)")
-plt.show()
-
-# seeing the depth array
-print("shape of depth array: ", depth.shape)    
-# rows len-> height of the image [in px], x= 1225 [example]
-# columns len-> width of image [in px], y= 593 [example]
-print("Min depth:", np.min(depth))
-print("Max depth:", np.max(depth))
-print("Mean depth:", np.mean(depth))
-
-# for some specific pixel
-y, x = 400, 500
-print(f"Depth at ({x},{y}):", depth[y][x], "meters")
-
-
-person_boxes= detect_people('testImage/test6.png')
 
 # getting the feet depth
 def get_feet_depth(depth_map, person_boxes, region_size=5):
@@ -86,8 +57,24 @@ def get_feet_depth(depth_map, person_boxes, region_size=5):
         feet_depth_list[i]= np.median(region)
 
     return feet_depth_list
- 
 
 
-feet_depths= get_feet_depth(depth, person_boxes)
-print("Feet depths: ", feet_depths)
+if __name__== '__main__':
+    depth, focallength_px= get_depth_image('testImage/test6.png')
+    person_boxes= detect_people('testImage/test6.png')
+    print("Focal length is: ", focallength_px)
+    # showing depth map
+    plt.figure(figsize=(10,5))
+    plt.title("Depth Map (meters)")
+    plt.imshow(depth, cmap='inferno')   # red = close, blue = far
+    plt.colorbar(label="Depth (m)")
+    plt.show()
+    print("shape of depth array: ", depth.shape)    
+    # rows len-> height of the image [in px], x= 1225 [example]
+    # columns len-> width of image [in px], y= 593 [example]
+    print("Min depth:", np.min(depth))
+    print("Max depth:", np.max(depth))
+    print("Mean depth:", np.mean(depth))
+
+    feet_depths= get_feet_depth(depth, person_boxes)
+    print("Feet depths: ", feet_depths) 
---
+++ pinhole.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.append(r'D:\Antares')  
+from PeopleDetection import detect_people
+from depthpro import get_depth_image, get_feet_depth
+import numpy as np 
+
+
+depth, focallength= get_depth_image('testImage/test6.png')
+person_boxes= detect_people('testImage/test6.png')
+feet_depths= get_feet_depth(depth, person_boxes)
+
+
+def pixel_to_3d(x_pixel, y_pixel, depth, fx, fy, cx, cy):
+    X = (x_pixel - cx) * depth / fx
+    Y = (y_pixel - cy) * depth / fy
+    Z = depth
+    return np.array([X, Y, Z])
+
+
+def get_3d_coordinates(person_boxes, feet_depths, focallength, depth):
+    h, w= depth.shape
+    fx= fy= focallength
+    cx= w/2
+    cy= h/2
+    position_3d={}
+
+    for person_id, feet_depth in feet_depths.items():
+        x1, y1, x2, y2= person_boxes[person_id]
+        feet_x= int((x1+x2)/2)
+        feet_y= int(y2)
+
+        coords= pixel_to_3d(feet_x, feet_y, feet_depth,fx,fy,cx,cy)
+        position_3d[person_id]= coords
+    
+    return position_3d
+
+if __name__=='__main__':
+    positions= get_3d_coordinates(person_boxes,feet_depths,focallength, depth)
+    for person_id, coords in positions.items():
+        print(f"Person {person_id} → X: {coords[0]:.2f}m, Y: {coords[1]:.2f}m, Z: {coords[2]:.2f}m")
+
+
+
+        
+
+        
