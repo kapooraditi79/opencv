b82209f084b749601c7fa02584bc3f644612a0b4
kapooraditi79 <thinktank7997@gmail.com>
2026-05-04 11:05:17 +0530

    getting feet depth of each person id

--- PeopleDetection.py
+++ PeopleDetection.py
@@ -1,40 +1,10 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
+import sys
+sys.path.append(r'D:\Antares')  
 
 model= YOLO("yolov10s.pt")
-cap= cv2.imread('testImage/test6.png')
-
-result= model(cap, classes=0)
-
-annotated_image= result[0].plot()
-cv2.imshow('People Detection', annotated_image)
-
-
-# extract boxes and IDs
-detections= result[0].boxes
-if detections is None:
-    print("No detections found")
-person_boxes= {}
-
-if detections is not None:
-    for i, detection in enumerate(detections):
-        x1, y1, x2, y2= detection.xyxy[0].cpu().numpy()
-
-        # get the tracking id
-        # track_id= int(detection.id.item())
-
-        # get confidence score 
-        conf= detection.conf.item()
-
-        person_boxes[i]= [x1, y1, x2, y2]
-
-print("This is the number of bounding boxes: ", len(person_boxes))
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
 
 # # for the video  
 # cap= cv2.VideoCapture('testVideo/test2.mp4')
@@ -55,3 +25,30 @@ cv2.destroyAllWindows()
 
 # now we will use the depth pro model
 
+
+
+def detect_people(image_path):
+    cap= cv2.imread(image_path)
+    result= model(cap, classes=0)
+    detections= result[0].boxes
+
+    if detections is None:
+        print('No detection found')
+        return None
+    person_boxes= {}
+
+    for i, detection in enumerate (detections):
+        x1, y1, x2, y2= detection.xyxy[0].cpu().numpy()
+        person_boxes[i]= [x1, y1, x2, y2]
+    print("Number of bounding boxes:", len(person_boxes))
+    return person_boxes
+
+if __name__== '__main__':
+    boxes= detect_people('testImage/test6.png')
+    cap= cv2.imread('testImage/test6.png')
+
+    result= model(cap, classes=0)
+    annotated_image= result[0].plot()
+    cv2.imshow('People Detection', annotated_image)
+    cv2.waitkey(0)
+    cv2.destroyAllWindows()
--- depthpro.py
+++ depthpro.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append(r'D:\Antares')  
+
 import depth_pro
 from PIL import Image       # for image handling
 import torch
 import matplotlib.pyplot as plt
 from depth_pro import create_model_and_transforms
 import numpy as np
+from PeopleDetection import detect_people
 
 # we will use gpu
 device= torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -58,3 +62,32 @@ y, x = 400, 500
 print(f"Depth at ({x},{y}):", depth[y][x], "meters")
 
 
+person_boxes= detect_people('testImage/test6.png')
+
+# getting the feet depth
+def get_feet_depth(depth_map, person_boxes, region_size=5):
+    feet_depth_list= {}
+    for i in person_boxes:
+        x1,y1, x2, y2= person_boxes[i]
+
+        # feet position
+        feet_x= int((x1+x2)/2)
+        feet_y= int(y2)  
+
+        # extract the small region around feet
+        half= region_size//2
+        h,w= depth_map.shape    
+        y_start= max(0, feet_y- half)
+        y_end= min(h, feet_y+ half+1)
+        x_start= max(0, feet_x - half)
+        x_end= min(w, feet_x+ half+1)
+        
+        region= depth_map[y_start:y_end, x_start:x_end]
+        feet_depth_list[i]= np.median(region)
+
+    return feet_depth_list
+ 
+
+
+feet_depths= get_feet_depth(depth, person_boxes)
+print("Feet depths: ", feet_depths)
